01248132c4ca1522fc2933b8c49a1ff5c3af5b77
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-09-16 22:50:20 +0500

    Добавил сохранение данных в csv

---
+++ src/main/kotlin/utils/NewsFileSaver.kt
@@ -0,0 +1,52 @@
+package org.example.utils
+
+import org.example.models.News
+import java.io.File
+import java.util.*
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
+
+object NewsFileSaver {
+
+    fun saveNews(path: String, news: Collection<News>) {
+        checkFilePath(path)
+
+        File(path).printWriter().use { writer ->
+            val (headers, propertyMapping) = generateCsvHeaderAndMapping(News::class)
+            writer.println(headers.joinToString(","))
+
+            news.forEach { newsItem ->
+                val values = headers.map { header ->
+                    val propertyName = propertyMapping[header]
+                    val property = News::class.memberProperties.find { it.name == propertyName }
+                    property?.get(newsItem)?.toString()?.replace("\n", " ") ?: ""
+                }
+                writer.println(values.joinToString(","))
+            }
+        }
+    }
+
+    private fun generateCsvHeaderAndMapping(clazz: kotlin.reflect.KClass<*>): Pair<List<String>, Map<String, String>> {
+        val headerFields = clazz.memberProperties
+            .map { it.name.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() } }
+
+        val propertyMapping = clazz.memberProperties
+            .associate { it.name.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() } to it.name }
+
+        return Pair(headerFields, propertyMapping)
+    }
+
+
+
+    private fun checkFilePath(path: String) {
+        val file = File(path)
+
+        if (file.exists()) {
+            throw IllegalArgumentException(illegalArgumentMsg(path))
+        }
+    }
+
+    private fun illegalArgumentMsg(path: String): String {
+        return "The file in the specified path already exists: $path"
+    }
+}
